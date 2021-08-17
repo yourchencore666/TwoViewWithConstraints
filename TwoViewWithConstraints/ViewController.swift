7b7a11b06a64a36b169d39719bc5737408361b2c
Artem Yurchenko <yourchencore@yandex.ru>
2021-08-17 14:16:40 +0200

    Initial Commit

---
+++ TwoViewWithConstraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViewWithConstraints
+//
+//  Created by user202124 on 17.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
